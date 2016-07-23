9ebe85473ade2a6402c917e448beb0d26e3f53c0
Rasit Demirel <rrasitt@gmail.com>
2016-07-23 16:28:47 +0300

    I am not ready button dispose eklendi.

--- src/db2/diagnostic/view/LoginScreen.java
+++ src/db2/diagnostic/view/LoginScreen.java
@@ -59,6 +59,11 @@ public class LoginScreen extends javax.swing.JFrame {
         });
 
         jButton2.setText("Im not ready");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -129,6 +134,10 @@ public class LoginScreen extends javax.swing.JFrame {
         }
 
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
